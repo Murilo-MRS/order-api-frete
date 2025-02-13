db916db9c4c46ecc4768f46258b7676809b824b8
Murilo Rodrigues Santana <murilosantana1993@gmail.com>
2025-02-13 11:09:32 -0300

    fix: corrigindo construtor e get/setter de User.java

--- src/main/java/com/template/project/models/entities/User.java
+++ src/main/java/com/template/project/models/entities/User.java
@@ -39,9 +39,10 @@ public class User {
   public User() {
   }
 
-  public User(Long id, String username, String password, Role role) {
+  public User(Long id, String name, String email, String password, Role role) {
     this.id = id;
-    this.name = username;
+    this.name = name;
+    this.email = email;
     this.password = password;
     this.role = role;
   }
@@ -60,6 +61,14 @@ public class User {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
   public String getPassword() {
