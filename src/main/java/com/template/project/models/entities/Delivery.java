7e90675922c14710bd41a61d5de43ce7a9fcdfdc
Murilo Rodrigues Santana <murilosantana1993@gmail.com>
2025-02-14 17:07:45 -0300

    feat: - add AuditingEntityListener em Delivery, User

--- src/main/java/com/template/project/models/entities/Delivery.java
+++ src/main/java/com/template/project/models/entities/Delivery.java
@@ -4,6 +4,7 @@ import com.template.project.models.enums.DeliveryStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -15,9 +16,11 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "deliveries")
+@EntityListeners(AuditingEntityListener.class)
 public class Delivery {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/template/project/models/entities/User.java
+++ src/main/java/com/template/project/models/entities/User.java
@@ -3,6 +3,7 @@ package com.template.project.models.entities;
 import com.template.project.models.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -15,9 +16,11 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
