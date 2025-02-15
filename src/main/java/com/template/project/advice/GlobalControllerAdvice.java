5cb75a9d0f67bb3fc75070646b9c1df9fa3eea55
Murilo Rodrigues Santana <murilosantana1993@gmail.com>
2025-02-15 11:30:59 -0300

    feat: - add GlobalControllerAdvice.handleMethodArgumentNotValidException

--- src/main/java/com/template/project/advice/GlobalControllerAdvice.java
+++ src/main/java/com/template/project/advice/GlobalControllerAdvice.java
@@ -3,16 +3,33 @@ package com.template.project.advice;
 import com.template.project.exceptions.NotFoundException;
 import com.template.project.exceptions.UserAlreadyExistsException;
 import com.template.project.models.dtos.ErrorDto;
+import com.template.project.models.dtos.ErrorDto.FieldError;
+import jakarta.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<ErrorDto> handleNotFoundException(NotFoundException exception) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto(exception.getMessage()));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors().stream()
+        .map(error -> new FieldError(error.getField(), error.getDefaultMessage()))
+        .toList();
+
+    return ResponseEntity.badRequest().body(new ErrorDto("Validation failed", fieldErrors));
   }
 
   @ExceptionHandler(UserAlreadyExistsException.class)
