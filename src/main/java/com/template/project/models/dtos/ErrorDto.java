d62f8bde8bedc62ffab3363c5e422c866eab28be
Murilo Rodrigues Santana <murilosantana1993@gmail.com>
2025-02-15 11:27:48 -0300

    refact: - refatorando contructo de ErrorDto

--- src/main/java/com/template/project/models/dtos/ErrorDto.java
+++ src/main/java/com/template/project/models/dtos/ErrorDto.java
@@ -1,4 +1,15 @@
 package com.template.project.models.dtos;
 
-public record ErrorDto (String message) {
+import java.util.List;
+
+public record ErrorDto(
+    String message,
+    List<FieldError> fieldErrors
+) {
+  public ErrorDto(String message) {
+    this(message, null);
+  }
+
+  public static record FieldError(String field, String error) {
+  }
 }
